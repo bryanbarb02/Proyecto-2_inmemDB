6acf4151c7766d36797f76022ac5ad6a7be6c47b
03-Kevin <43195779+03-Kevin@users.noreply.github.com>
2019-05-16 19:02:00 -0600

    Add files via upload

---
+++ Cliente/src/PaqueteDatos/Cliente/src/PaqueteDatos/DatoColumna.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PaqueteDatos;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ *
+ * @author Fallas
+ */
+public class DatoColumna {
+    List Esquema=null;
+    public void ObtenerLista (List esquema){
+        System.out.println(esquema);
+        Esquema = esquema;
+        System.out.print(Esquema);
+    }
+    
+}
+
