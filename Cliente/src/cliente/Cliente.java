3c8f05f9476c7edf5716cd1fdf9b18f86b74fe93
bryanbarb02 <37256420+bryanbarb02@users.noreply.github.com>
2019-05-22 02:07:25 -0600

    Update Cliente.java

--- Cliente/src/cliente/Cliente.java
+++ Cliente/src/cliente/Cliente.java
@@ -105,14 +105,14 @@ public class Cliente  implements Runnable{
                     }
                     
                     
-                    if (reference.getReference().equals("NombreEsquema")){
+                    if (reference.getReference().equals("NombreColumna")){
                         System.out.println("Client recieved a server response: DotConnectionPack");
                         DatoColumna reciveColumnas = JSONUtil.convertJsonToJava(recievedObjectAsString, DatoColumna.class);
                         columna.getEsquema();
                         break;
                     }
                     
-                    if (reference.getReference().equals("NombreColumna")){
+                    if (reference.getReference().equals("NombreEsquema")){
                         System.out.println("Client recieved a server response: DotConnectionPack");
                         DatoFila reciveFilas = JSONUtil.convertJsonToJava(recievedObjectAsString, DatoFila.class);
                         //fila.ObtenerLista(reciveFilas.getEsquema());
